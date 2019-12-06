fa59490e04b86639f939ea2030547c162b05afbd
TheSkyentist <raphael.hviding@gmail.com>
2019-12-06 13:58:41 -0700

    Added picture processing script

--- gallery.py
+++ gallery.py
@@ -2,26 +2,49 @@
 
 # Packages
 import os
-# Import Python Image Library
+import sys
 from PIL import Image
 
+# Path to gallery
 gallerypath = 'images/gallery/'
 
-# Wrapper for image
-def imwrap(path):
+# HTML file
+htmlsection = '<!-- Gallery -->\n'
 
-    return '<div class="col-4"><span class="image fit"><img src="images/gallery/' + path + ' alt="" /></span></div>'
+# Iterate over images in gallery that are not final products
+for i,impath in enumerate([x for x in os.listdir(gallerypath) if (not 'Gallery' in x)]):
 
-def openim(path):
+    # Load Photo
+    try:
+        img = Image.open(gallerypath+impath)
+    except: 
+        print('Unable to load',gallerypath+impath,'.')
+        print('Please ensure that this is an image file.')
+        print('Note, not all image formats are supported.')
+        print('If this is an image, please convert to JPG or PNG.')
+        sys.exit(1)
+    
+    # Crop photo
+    size = min(img.size)
+    width, height = img.size
+    img = img.crop(((width-size)/2,(height-size)/2,(width+size)/2,(height+size)/2))
 
-    if 
+    # Save photo
+    imname = gallerypath+'Gallery'+str(i)+'.jpeg'
+    img.save(imname,format='JPEG')
 
-for impath in os.listdir(gallerypath):
-    if 'Gallery' not in impath:
-        print(gallerypath+impath)
-        x = Image.open(gallerypath+impath)
-        x.show()
+    # HTML addition
+    htmlsection += '\t'*11 + '<div class="col-4"><span class="image fit"><img src="' + imname + '" alt="" /></span></div>\n'
 
-# Open press
-file = open('press.html','r')
+# Open and edit press html
+file = open('press.html','r+')
 html = file.read().split('<!-- Gallery -->') # Read and split file
+
+# Add our section and close it with the tag
+html[1] = htmlsection+'\t'*11+'<!-- Gallery -->'
+
+# Find beggining of file and write
+file.seek(0)
+file.write(''.join(html))
+file.truncate() # Throw away the rests
+file.close()
